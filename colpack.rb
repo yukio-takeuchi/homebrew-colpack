d67b434c3aafc1ef3d1d5166196fc1ad91ebadd9
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-19 14:23:05 +0900

    Initial commit

---
+++ colpack.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Colpack < Formula
+  desc "ColPack"
+  homepage "https://cscapes.cs.purdue.edu/coloringpage/index.htm"
+  url "https://github.com/CSCsw/ColPack/archive/v1.0.10.tar.gz"
+  head "https://github.com/CSCsw/ColPack.git"
+
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system  "cd", "build/automake"
+    #system "pwd"
+    system "autoreconf", "-v", "--install", "--force"
+    # system "pwd"
+    system  "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test colpack`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
